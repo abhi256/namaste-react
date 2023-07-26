34590725a0e87c291eade60ea1f10bae739051b7
abhi256 <abhishek2561997mishra@gmail.com>
2023-07-27 01:05:41 +0530

    main

--- App.js
+++ App.js
@@ -1,5 +1,34 @@
+/**
+ *  <div id="parent">
+        <div id="child">
+            <h1> i am h1 tag</h1>
+        </div>
 
-const heading = React.createElement("h1",{id:"heading",xyz:"abc"},"Hello World From React!!");
+    </div>
+ * 
+ * */ 
+
+    const heading = React.createElement(
+    "div",
+    {id:"parent"},[
+    React.createElement(
+        "div",
+        {id:"child"},
+        [ React.createElement("h1",{},"I am the h1 tag"),
+        React.createElement("h2",{},"I am the h2 tag") 
+    ]),
+    React.createElement(
+        "div",
+        {id:"child2"},
+        [ React.createElement("h1",{},"I am the h1 tag"),
+        React.createElement("h2",{},"I am the h2 tag") 
+    ])
+]);
+
+
+
+
+//const heading = React.createElement("h1",{id:"heading",xyz:"abc"},"Hello World From React!!");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
